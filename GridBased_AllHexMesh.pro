47958376d43b2a394648ae18bb89a4d21ec2ab60
DanyOrtu97 <danieleortu8@gmail.com>
2021-04-11 11:11:09 +0200

    Added bb creation, still in library's testing phase

--- GridBased_AllHexMesh.pro
+++ GridBased_AllHexMesh.pro
@@ -39,7 +39,7 @@ DEFINES += CINOLIB_USES_QT
 QT += core opengl
 INCLUDEPATH +=/home/tesi/Scrivania/cinolib/include
 INCLUDEPATH +=/home/tesi/Scrivania/cinolib/external/eigen
-DATA_PATH       = \\\"$$PWD/../cinolib/examples/data/\\\"
+DATA_PATH       = \\\"$$PWD/../Geometries/2019/input/smooth/\\\"
 DEFINES        += DATA_PATH=$$DATA_PATH
 
 # just for Linux
--- main.cpp
+++ main.cpp
@@ -4,22 +4,39 @@
 #include <cinolib/gui/qt/qt_gui_tools.h>
 #include <cinolib/meshes/meshes.h>
 #include <cinolib/color.h>
+#include <cinolib/geometry/drawable_aabb.h>
 
 
 int main(int argc, char *argv[])
 {
 
+
     using namespace cinolib;
     QApplication a(argc, argv);
+    //std::string s = (argc>1) ? std::string(argv[1]) : std::string(DATA_PATH) + "/airplane1_input_tri.obj";
 
-    std::string s = (argc==2) ? std::string(argv[1]) : std::string(DATA_PATH) + "/rockerarm.mesh";
+    std::vector<vec3d> list(4);
+    std::vector<AABB> bb;
 
-    DrawableHexmesh<> hex(s.c_str());
+    vec3<double> v1(1., 1., 1.);
+    vec3<double> v2(1., 4., 1.);
+    vec3<double> v3(4., 4., 1.);
+    vec3<double> v4(4., 1., 1.);
 
-    hex.edge_set_color(Color::BLUE());
-    GLcanvas gui;
-    gui.push_obj(&hex);
-    gui.show();
+    list[0] = v1;
+    list[1] = v2;
+    list[2] = v3;
+    list[3] = v4;
+
+    AABB tree(list);
+
+    bb.push_back(tree);
+
+    DrawableAABB m(bb);
+
+    GLcanvas canvas;
+    canvas.push_obj(&m);
+    canvas.show();
 
 
     // CMD+1 to show mesh controls.
